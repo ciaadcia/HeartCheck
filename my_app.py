3b0fc292ca24a5adcfd66ac453487925a53ad57b
ciaa <154084369+ciaadcia@users.noreply.github.com>
2026-01-28 15:42:31 +0700

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,28 +1,29 @@
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer, QTime, QLocale
+from PyQt5.QtGui import QDoubleValidator, QIntValidator, QFont 
 from PyQt5.QtWidgets import (
-    QApplication, QWidget,
-    QHBoxLayout, QVBoxLayout,
-    QGroupBox, QRadioButton,
-    QPushButton, QLabel, QListWidget, QLineEdit)
+        QApplication, QWidget,
+       QHBoxLayout, QVBoxLayout, QGridLayout,
+       QGroupBox, QRadioButton,
+       QPushButton, QLabel, QListWidget, QLineEdit)
 from instr import *
 from second_win import *
 
 class MainWin(QWidget):
     def __init__(self):
         super().__init__()
+        self.set_appear()
         self.initUI()
         self.connects()
-        self.set_appear()
         self.show()
     def initUI(self):
-        self.btn_next = QPushButton(txt_next, self)
+        self.btn_next = QPushButton(txt_next)
         self.hello_text = QLabel(txt_hello)
         self.instruction = QLabel(txt_instruction)
-        self.layout_line = QVBoxLayout()
-        self.layout_line.addWidget(self.hello_text, alignment=Qt.AlignLeft)
-        self.layout_line.addWidget(self.instruction, alignment=Qt.AlignLeft)
-        self.layout_line.addWidget(self.btn_next, alignment=Qt.AlignCenter)
-        self.setLayout(self.layout_line)
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.hello_text, alignment = Qt.AlignLeft)
+        self.layout.addWidget(self.instruction, alignment = Qt.AlignLeft)
+        self.layout.addWidget(self.btn_next, alignment = Qt.AlignCenter)         
+        self.setLayout(self.layout)
     def next_click(self):
         self.tw = TestWin()
         self.hide()
@@ -32,7 +33,8 @@ class MainWin(QWidget):
         self.setWindowTitle(txt_title)
         self.resize(win_width, win_height)
         self.move(win_x, win_y)
-app = QApplication([])
-mw = MainWin()
-app.exec_()
-
+def main():
+    app = QApplication([])
+    mw = MainWin()
+    app.exec_()
+main()
